002dae1f564a8787d1dc8346e212c811a609f488
Aleksander Motuzov <motuzov@gmail.com>
2022-09-03 23:06:58 +0300

    parameter kernel_name was removed

--- runner.py
+++ runner.py
@@ -6,8 +6,8 @@ def run_notebook(
         out_nb_name,
         parameters
         ):
+
     nn = papermill.execute.execute_notebook(
         temp_nb_name,
         out_nb_name,
-        kernel_name='Python3',
         parameters=parameters)
