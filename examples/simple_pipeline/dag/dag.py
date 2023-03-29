01d6fa9ffd3baefcb0a7004f363e51077056094f
Aleksander Motuzov <motuzov@gmail.com>
2023-03-29 12:28:09 +0000

    using of the new interface

--- examples/simple_pipeline/dag/dag.py
+++ examples/simple_pipeline/dag/dag.py
@@ -1,15 +1,8 @@
-from dsml4s8e.extract_dag_params_from_nb import get_dagstermill_op_params
+from dsml4s8e.op_params_from_nb import get_dagstermill_op_params
 from dagstermill import define_dagstermill_op, local_output_notebook_io_manager
 from dagster import (
-    asset,
-    AssetIn,
-    Definitions,
-    file_relative_path,
     job,
     op,
-    get_dagster_logger,
-    Field,
-    Out,
     In
 )
 from pathlib import Path
@@ -22,7 +15,6 @@ def full_path(path: str) -> str:
 op_1_params = get_dagstermill_op_params(full_path("../data_load/nb_1.ipynb"))
 op1 = define_dagstermill_op(**op_1_params,
                             save_notebook_on_failure=True)
-
 
 op_2_params = get_dagstermill_op_params(full_path("../data_load/nb_2.ipynb"))
 op2 = define_dagstermill_op(**op_2_params,
@@ -41,6 +33,19 @@ def final_op(context, data2: str):
     # context.log.info(output_notebook_name)
 
 
+@op(
+    description="""start op""",
+    ins={
+    },
+    out={},
+)
+def start(context):
+    import time
+    time.sleep(3)
+    context.log.info('log')
+    # context.log.info(output_notebook_name)
+
+
 @job(
     name='simple_pipeline',
     tags={"cdlc_stage": "dev"},
@@ -52,3 +57,8 @@ def dagstermill_pipeline():
     res_urls = op1()
     res = op2(*res_urls[:-1])
     final_op(*res[:-1])
+    # start()
+
+
+if __name__ == "__main__":
+    result = dagstermill_pipeline.execute_in_process()
