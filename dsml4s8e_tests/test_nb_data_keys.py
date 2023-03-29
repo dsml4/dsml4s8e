273d9bf5f57acae5511c8307efe64d8bdc01dbe7
Aleksander Motuzov <motuzov@gmail.com>
2023-03-29 12:17:03 +0000

    tests fixind

--- dsml4s8e_tests/test_nb_data_keys.py
+++ dsml4s8e_tests/test_nb_data_keys.py
@@ -5,9 +5,7 @@ def test_nb_data_keys():
     nb_data_keys_ = nb_data_keys.NotebookDataKeys(
         ins_data_key_dag_name={'data_key': 'alias_data_key'},
         outs=['data1'],
-        nb_path='/home/jovyan/work/dev/pipeline_example/data_load/dagstermill.ipynb'
+        op_id='pipeline_example.data_load.dagstermill'
     )
-    assert (nb_data_keys_.id ==
-            'pipeline_example.data_load.dagstermill')
     assert (nb_data_keys_.outs.keys[0] ==
             'pipeline_example.data_load.dagstermill.data1')
