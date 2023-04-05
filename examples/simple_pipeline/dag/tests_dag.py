3e404f5dffe0fae074456a9b06c8767b58b002e7
Aleksander Motuzov <motuzov@gmail.com>
2023-04-05 18:15:01 +0000

    the script parametrization

--- examples/simple_pipeline/dag/tests_dag.py
+++ examples/simple_pipeline/dag/tests_dag.py
@@ -4,21 +4,17 @@ from pathlib import Path
 from dagster import reconstructable
 from dagster._core.execution.api import execute_pipeline
 from dagster._cli.utils import get_instance_for_service
+import yaml
+from yaml.loader import SafeLoader
+import sys
 
 
 if __name__ == "__main__":
     # do_execute_command(pipeline, instance, config, mode, tags, solid_selection, preset)
-    config = {
-        'ops': {
-            'nb_2': {
-                'config': {
-                    'b': 300},
-                'inputs': {
-                    'url_nb_1_data1': '/home/jovyan/work/dagster_home/storage/f95df29a-95b9-4eb9-9b51-fb10c8cb5f7c/nb_1/data1'
-                    }
-                }
-            }
-        }
+    sys.argv[1]
+    print(sys.argv[1])
+    with open('nb_2.yaml') as f:
+        config = yaml.load(f, Loader=SafeLoader)
 
     with get_instance_for_service("``dagster job execute``") as instance:
         pipeline = reconstructable(dagstermill_pipeline)
@@ -31,4 +27,21 @@ if __name__ == "__main__":
         print(results)
         print(results.run_id)
         print(results.node_result_list[0].output_values['url_nb_2_data2'])
+
+        storage_dict = f'{instance.root_directory}/storage'
+        nb_name = list(config['ops'].keys())[0]
+        output_var_name = list(
+            results.pipeline_def.graph.node_dict['nb_2'].output_dict.keys()
+            )[0]
+
+        local_alias = output_var_name[len('url_') + len(nb_name) + len('_'):]
+
+        (res_output_values,
+         output_values
+         ) = (
+            results.node_result_list[0].output_values[output_var_name],
+            f'{storage_dict}/{results.run_id}/{nb_name}/{local_alias}'
+        )
+
+        print(res_output_values == output_values)
         print(results.success)
