818ce7677a6672a22dc0ef98b1add92d74635fa0
Aleksander Motuzov <motuzov@gmail.com>
2023-04-10 15:25:41 +0000

    renaming: urls->paths

--- dsml4s8e_tests/test_do_dotted_paths.py
+++ dsml4s8e_tests/test_do_dotted_paths.py
@@ -1,4 +1,4 @@
-from dsml4s8e import dotted_urls_names
+from dsml4s8e import dotted_catalog_path
 
 
 def test_reduce_leafs():
@@ -8,7 +8,7 @@ def test_reduce_leafs():
         'pip2.comp2.nb2.entity2': 'url_entity2',
         'pip2.comp2.nb2.entity3': 'url_entity3'
     }
-    res = dotted_urls_names._reduce_leafs(id_urls)
+    res = dotted_catalog_path._reduce_leafs(id_urls)
     assert (res['pip1.comp1.nb1'] == [('entity1', 'url_entity1')])
     assert (res['pip1.comp1.nb2'] == [('entity2', 'url_entity2')])
     assert (res['pip2.comp2.nb2'] == [('entity2', 'url_entity2'),
@@ -22,24 +22,24 @@ def test_pack_leafs_list_to_obj():
         'pip2.comp2.nb2.entity2': 'url_entity2',
         'pip2.comp2.nb2.entity3': 'url_entity3'
     }
-    path_entitie_list = dotted_urls_names._reduce_leafs(id_urls)
-    path_entitie_obj = dotted_urls_names._pack_leafs_list_to_obj(path_entitie_list)
+    path_entitie_list = dotted_catalog_path._reduce_leafs(id_urls)
+    path_entitie_obj = dotted_catalog_path._pack_leafs_list_to_obj(path_entitie_list)
     assert (path_entitie_obj['pip2.comp2.nb2'].entity2 == 'url_entity2')
     obj_class_name = type(path_entitie_obj['pip2.comp2.nb2']).__name__
     assert (obj_class_name == 'Pip2Comp2Nb2')
 
 
-def test_do_dotted_urls_names():
+def test_do_dotted_paths():
     key_urls = {
         'pip1.comp1.nb1.entity1': 'url_entity1',
         'pip1.comp1.nb2.entity2': 'url_entity2',
         'pip2.comp2.nb2.entity2': 'url_entity2',
         'pip2.comp2.nb2.entity3': 'url_entity3'
     }
-    comp_urls = dotted_urls_names.do_dotted_urls_names(key_urls)
+    comp_urls = dotted_catalog_path.do_dotted_paths(key_urls)
     assert (comp_urls.pip1.comp1.nb1.entity1 == 'url_entity1')
     assert (comp_urls.pip1.comp1.nb2.entity2 == 'url_entity2')
     assert (comp_urls.pip2.comp2.nb2.entity2 == 'url_entity2')
     assert (comp_urls.pip2.comp2.nb2.entity3 == 'url_entity3')
 
-    assert (not dotted_urls_names.do_dotted_urls_names([]))
+    assert (not dotted_catalog_path.do_dotted_paths([]))
