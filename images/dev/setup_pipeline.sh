a8a529559713b4874fe17753459e6325238b617b
motuzov <motuzov_av@nlmk.com>
2023-06-25 08:26:37 +0000

    dev tools: black formatting

--- images/dev/setup_pipeline.sh
+++ images/dev/setup_pipeline.sh
@@ -4,4 +4,5 @@ pip install --editable .
 cd /home/jovyan/work/dsml4s8e/examples/simple_pipeline/dag
 bash run.sh&
 cd
+code --install-extension ms-python.python
 start.sh jupyter lab --LabApp.token=''
