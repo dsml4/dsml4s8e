68f28da5bcfd86537ba558565b227c6e4c53b440
motuzov <motuzov@gmail.com>
2025-02-09 20:13:54 +0000

    nb's tags validatio

--- dsml4s8e/op_params_from_nb.py
+++ dsml4s8e/op_params_from_nb.py
@@ -25,17 +25,28 @@ def nb_outs2dagster_outs(outs, nb_id):
     return {kye2pathvar_name(k): Out(str) for k in nb_data.outs.keys}
 
 
+class MissingTagsException(Exception):
+    def __init__(self, nb_path, tags):
+        self.tags = tags
+        self.nb_path = nb_path
+
+    def __str__(self):
+        return f"Missing tags: {self.tags} in {self.nb_path}"
+
+
 def dagstermill_op_params_from_nb(nb_path: str):
     nb = nbformat.read(nb_path, as_version=4)
+    nb_tags = set()
     params = None
     for cell in nb.cells:
-        tags = []
-        if cell.cell_type == "code":
-            tags = get_cell_tags(cell)
-        if "op_parameters" in tags:
+        cell_tags = get_cell_tags(cell)
+        nb_tags.update(cell_tags)
+        if "op_parameters" in cell_tags:
             op.NbOp.set_current_nb_path(nb_path)
-            # In this cell the object of NbOp is creaded
-            # in NbOp.__init__
+            # In this cell, the object of NbOp is created,
+            # and its parameters saved in NbOp._all_params
+            # to be available via the static method op.NbOp.params()
+            # see NbOp.__init__ and op.NbOp.params()
             exec(cell.source)
             params = op.NbOp.params()
             if "ins" in params:
@@ -50,5 +61,8 @@ def dagstermill_op_params_from_nb(nb_path: str):
             params["output_notebook_name"] = f"out_{params['name']}"
             local_path = "/".join(nb_path.split("/")[-2:])
             params["description"] = f"path: {local_path}"
-            return params
-    return params
+    mandatory_tags = {"op_parameters", "parameters"}
+    dsml_nb_tags = mandatory_tags.intersection(nb_tags)
+    if dsml_nb_tags == mandatory_tags:
+        return params
+    raise MissingTagsException(nb_path, mandatory_tags - dsml_nb_tags)
--- dsml4s8e_tests/test_op_params_from_nb.py
+++ dsml4s8e_tests/test_op_params_from_nb.py
@@ -6,44 +6,42 @@ from dagster import Out, In
 
 def test_op_params_from_nb():
     test_dir = Path(__file__).parent
-    nb_path = f'{test_dir}/notebooks/nb_with_params.ipynb'
+    nb_path = f"{test_dir}/notebooks/nb_with_params.ipynb"
     op_params = op_params_from_nb.dagstermill_op_params_from_nb(nb_path)
 
     etalon_op_params = {
-        'config_schema': {
-            "a": Field(
-                int,
-                description='nb params',
-                default_value=10
-                )
-        },
-        'description': """path: notebooks/nb_with_params.ipynb""",
-        'ins': {'pip1.comp1.nb1.data1': 'alias_data_key'},
-        'outs': ['data1']
+        "config_schema": {"a": Field(int, description="nb params", default_value=10)},
+        "description": """path: notebooks/nb_with_params.ipynb""",
+        "ins": {"pip1.comp1.nb1.data1": "alias_data_key"},
+        "outs": ["data1"],
     }
-    comp_keys = [
-        'description'
-
-    ]
+    comp_keys = ["description"]
     for k in comp_keys:
-        assert (op_params[k] == etalon_op_params[k])
+        assert op_params[k] == etalon_op_params[k]
     # pylint: disable-next=line-too-long
     assert (
-        etalon_op_params['config_schema']['a'].default_value ==
-        op_params['config_schema']['a'].default_value)
+        etalon_op_params["config_schema"]["a"].default_value
+        == op_params["config_schema"]["a"].default_value
+    )
+
+
+def test_missing_nb_tags():
+    test_dir = Path(__file__).parent
+    nb_path = f"{test_dir}/notebooks/empty.ipynb"
+    try:
+        _ = op_params_from_nb.dagstermill_op_params_from_nb(nb_path)
+    except op_params_from_nb.MissingTagsException as e:
+        assert e.tags == {"op_parameters", "parameters"}
 
 
 def test_nb_ins2dagster_ins():
-    ins = {
-        'pip1.comp1.nb1.data1': 'comp1_nb1_data1'
-        }
+    ins = {"pip1.comp1.nb1.data1": "comp1_nb1_data1"}
     dagster_ins = op_params_from_nb.nb_ins2dagster_ins(ins)
-    assert (dagster_ins['comp1_nb1_data1'] == In(str))
+    assert dagster_ins["comp1_nb1_data1"] == In(str)
 
 
 def test_nb_outs2dagster_outs():
-    outs = ['data1']
-    nb_id = 'pipeline_example.nb'
+    outs = ["data1"]
+    nb_id = "pipeline_example.nb"
     dagster_ins = op_params_from_nb.nb_outs2dagster_outs(outs, nb_id)
-    assert (dagster_ins['path_nb_data1'] ==
-            Out(str))
+    assert dagster_ins["path_nb_data1"] == Out(str)
