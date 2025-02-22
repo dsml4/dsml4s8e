3d6c99bb6bb36da16d0f5f4acecb9427153a06ba
motuzov <motuzov@gmail.com>
2025-02-22 17:20:41 +0000

    bugfix: using get_fake_context

--- dsml4s8e_tests/test_dagster_home_catalog.py
+++ dsml4s8e_tests/test_dagster_home_catalog.py
@@ -1,4 +1,4 @@
-from .context_mock import get_context_mock
+from .fake_context import get_fake_context
 
 from dsml4s8e.dsmlcatalog import dagster_home
 from dsml4s8e import data_keys
@@ -6,7 +6,7 @@ import os
 
 
 def test_dagster_home():
-    context = get_context_mock("0000")
+    context = get_fake_context("0000")
     dagcat = dagster_home.DagsterStorageCatalog(context)
     k = "simple_pipeline.data_load.op_nb_1.data1"
     dk = data_keys.DataKyes([k])
