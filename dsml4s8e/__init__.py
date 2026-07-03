ad68824c8ba3570a0fe7e6c6bc6629d39c51a9f4
motuzov <motuzov@gmail.com>
2026-07-03 19:06:05 +0000

    the api

--- dsml4s8e/__init__.py
+++ dsml4s8e/__init__.py
@@ -1,1 +1,8 @@
 __version__ = "0.1.5"
+
+from dsml4s8e.op_params_from_nb import (
+    define_dagstermill_op_kvargs_from_nb as define_dagstermill_op_kvargs_from_nb,
+    MissingTagsException as MissingTagsException,
+)
+
+from dsml4s8e.nb_op import NbOp as NbOp
